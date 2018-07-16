0663f623ace79cae438827b6b76f86df434d1325
IamDLY <1219642339@qq.com>
2018-07-16 14:53:53 +0800

    主页添加书架模块

--- src/pages/home.js
+++ src/pages/home.js
@@ -30,6 +30,19 @@ class _HomePage extends React.Component {
         <SearchBar
           handleSearchInputClick={() => this.toSearchPage()}
         />
+
+        <p className="module-title">书架</p>
+
+        <ul className="native-novel-list">
+          <li className="native-novel-item">
+            <img className="cover" src="http://statics.zhuishushenqi.com/agent/http%3A%2F%2Fimg.1391.com%2Fapi%2Fv1%2Fbookcenter%2Fcover%2F1%2F2126166%2F2126166_c1002339e5664d7bb1a536e2608fa599.jpg%2F" alt=""/>
+            <section className="novel-info bottom-border">
+              <p className="title">圣墟</p>
+              <span className="tip">10小时前:第1140章 追杀</span>
+            </section>
+            <span className="novel-update">更新</span>
+          </li>
+        </ul>
       </div>
     );
   }
